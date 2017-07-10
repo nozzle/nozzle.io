995ef2ab9ff8bdd8aef7dbe32ca64a85b44be36b
Tanner Linsley <tannerlinsley@gmail.com>
2017-07-10 12:08:22 -0600

    Glamorous

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
+import { rehydrate } from 'glamor'
 //
 import Head from '../src/components/Head'
 import Page from '../src/components/Page'
+
+// Adds server generated styles to glamor cache.
+// Has to run before any `style()` calls
+// '__NEXT_DATA__.ids' is set in '_document.js'
+if (typeof window !== 'undefined') {
+  rehydrate(window.__NEXT_DATA__.ids)
+}
 
 export default class Index extends Component {
   render () {

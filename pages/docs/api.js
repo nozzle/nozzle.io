a85245e41ebcc5331d4359f8edc1336cceb860f9
Tanner Linsley <tannerlinsley@gmail.com>
2020-01-15 08:00:19 -0700

    Update api.js

--- pages/docs/api.js
+++ pages/docs/api.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
-import styled from 'styled-components'
-import Link from 'next/link'
 //
 
 import Head from 'components/Head'
-
-import { H1, H2, H3, H4, H6, P, Img, Button } from 'components/Html'
 
 const SwaggerUI = dynamic(() => import('swagger-ui-react'), { ssr: false })
 
@@ -15,7 +11,7 @@ export default function Api() {
     <div>
       <Head title="API Documentation - Nozzle - Search Engine Keyword Ranking Software" />
       <main>
-        <SwaggerUI url="/static/api.swagger.json" />
+        <SwaggerUI url="https://storage.googleapis.com/nozzle-api/swagger.json" />
       </main>
     </div>
   )

2b29b1db3f2cd1ae6fd16c5c50929a6d41d60f01
boydnorwood <52715622+boydnorwood@users.noreply.github.com>
2020-03-31 22:58:39 -0600

    updated alt text of customized scheduling image
    Changed it to "Keyword rank with customized scheduling feature" to help us move up in rankings for "keyword rank tracker". This page currently ranks #14 for this phrase.

--- pages/index.js
+++ pages/index.js
@@ -487,7 +487,7 @@ export default class Home extends Component {
             </P>
             <Centered>
               <Div>
-                <Img src={require('public/img/schedules.png')} alt="Use customized scheduling for each keyword group to track your rankings" />
+                <Img src={require('public/img/schedules.png')} alt="Keyword rank tracker with customized scheduling feature" />
               </Div>
             </Centered>
             <Centered>

6c88236abba5a29d4f77afad47e77d890668b80c
Jake Tribe <54113103+jtribe32@users.noreply.github.com>
2020-07-01 10:37:57 -0600

    Creates tests with Cypress (#86)
    * Creates tests with Cypress
    
    fixes #65
    
    * Create package-lock.json
    
    * Delete package-lock.json
    
    * adds Xvfb dependency
    
    * Update package.json
    
    * Moves the tests from vercel to github CI
    
    * Update tests.js
    
    * Update tests.yml
    
    * Update tests.yml
    
    * Update tests.yml
    
    * Update tests.yml
    
    * Update tests.yml
    
    * Update tests.yml
    
    * Update tests.yml
    
    * Update tests.yml
    
    * Update tests.yml
    
    Co-authored-by: Tanner Linsley <tannerlinsley@gmail.com>

---
+++ cypress/integration/tests.js
@@ -0,0 +1,31 @@
+describe('Components are showing up in Post', () => {
+  it('AuthorsAndContributors Component is rendering', () => {
+    window.cy.visit('/blog/seo-humor-from-the-twitterverse/')
+    window.cy.get('.AuthorsAndContributors__Container-sc-1wovwrc-0')
+  })
+  it('RelatedPosts Component is rendering', () => {
+    window.cy.get('.RelatedPosts__Post-gudoks-2').should('have.length', 3)
+  })
+  it('Tweets are centered and shown as embeds', () => {
+    //Each tweet has class of twitter-tweet which centers it
+    window.cy.get('.twitter-tweet').should('have.length', 39)
+
+    //Each tweet is shown as a embedded tweet
+    window.cy.get('.twitter-tweet-rendered').should('have.length', 39)
+  })
+})
+
+describe('SiteMap and robots.txt ', () => {
+  it('SiteMap was generated', () => {
+    window.cy.readFile('public/sitemap.xml')
+  })
+
+  it('robots.txt does not disallow our site', () => {
+    window.cy
+      .readFile('public/robots.txt')
+      .should(
+        'eq',
+        'User-agent: *\nDisallow: /l\nsitemap: https://nozzle.io/sitemap.xml'
+      )
+  })
+})

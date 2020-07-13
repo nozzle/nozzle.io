3c88cc5ecd012a5ad5418ec0df1e40eedfa39de8
jake tribe <jacobtribe@me.com>
2020-07-13 14:40:35 -0600

    Update metrics.js

--- pages/metrics.js
+++ pages/metrics.js
@@ -12,6 +12,8 @@ import {
 import Head from 'components/Head'
 import { angle } from 'utils/Styles'
 import styled, { css } from 'styled-components'
+import Smackdown from 'components/Smackdown'
+import { Container } from 'components/Layout'
 
 const section = css`
   padding: 7% 15%;
@@ -23,32 +25,6 @@ const Top = styled('section')`
   background: ${props => props.theme.colors.primaryDarker};
   color: white;
 `
-// const Metric = styled('div')`
-//   height: auto;
-//   position: relative;
-//   margin-bottom: 3rem;
-
-//   .first {
-//   }
-//   .second {
-//     background: ${props => props.theme.colors.primaryDarker};
-//     color: white;
-//   }
-// `
-// const Thumbnail = styled('img')`
-//   margin: 2rem;
-//   display: inline-block;
-//   border-radius: 1rem;
-//   border: 0.15rem solid ${props => props.theme.colors.primaryDarker};
-//   height: 10rem;
-//   width: auto;
-// `
-// const Text = styled('div')`
-//   display: inline-block;
-//   position: absolute;
-//   top: 2rem;
-//   max-width: 800px;
-// `
 export async function getServerSideProps(req) {
   const props = await fetchMetrics()
   return {
@@ -77,33 +53,40 @@ export default function Metrics({ metrics }) {
               Nozzle has you covered.
             </Text>
           </Top>
-          <Stack spacing={5}>
-            {metrics.map(metric => {
-              return (
-                <Box p={4} display={{ md: 'flex' }} key={metric.fields.fieldId}>
-                  <Box flexShrink="0">
-                    <Image
-                      rounded="lg"
-                      width={{ sm: 250, md: 40 }}
-                      border
-                      shadow="md"
-                      src={metric.fields.thumbnail.fields.file.url}
-                      alt={metric.fields.name}
-                      ml={{ sm: '20%', md: '0' }}
-                    />
+
+          <Container>
+            <Stack spacing={5}>
+              {metrics.map(metric => {
+                return (
+                  <Box
+                    p={4}
+                    display={{ md: 'flex' }}
+                    key={metric.fields.fieldId}
+                  >
+                    <Box flexShrink="0">
+                      <Image
+                        rounded="lg"
+                        width={{ base: 250, md: 40 }}
+                        border
+                        shadow="md"
+                        src={metric.fields.thumbnail.fields.file.url}
+                        alt={metric.fields.name}
+                      />
+                    </Box>
+                    <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
+                      <Heading display="block" as="xl" fontWeight="500">
+                        {metric.fields.name}
+                      </Heading>
+
+                      <Text mt={2} color="gray.500">
+                        <Smackdown source={metric.fields.shortDescription} />
+                      </Text>
+                    </Box>
                   </Box>
-                  <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }} algin="center">
-                    <Heading display="block" as="h2" fontWeight="semibold">
-                      {metric.fields.name}
-                    </Heading>
-                    <Text mt={2} color="gray.500" maxW="1000px">
-                      {metric.fields.shortDescription}
-                    </Text>
-                  </Box>
-                </Box>
-              )
-            })}
-          </Stack>
+                )
+              })}
+            </Stack>
+          </Container>
         </div>
       ) : (
         ''

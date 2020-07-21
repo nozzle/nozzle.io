034cb69804010412810cb51cbc18ca7db6c1914f
Jake Tribe <54113103+jtribe32@users.noreply.github.com>
2020-07-21 10:49:14 -0600

    Tailwind utility (#103)
    * metrics page
    
    * Update metrics.js
    
    * Uses Tailwindcss
    
    * Adds anchor links to each section
    
    * Creates more space at top
    
    * Update top text & meta description
    
    * Style Changes
    
    Adds Header Image and 2 columns
    
    * Cleans up code
    
    Changes from styled components to more tailwinds

--- pages/metrics.js
+++ pages/metrics.js
@@ -16,15 +16,31 @@ const Top = styled('section')`
   ${section};
   ${angle('left')};
   background: ${props => props.theme.colors.primaryDarker};
-  color: white;
-  margin-bottom: 2rem;
-`
 
+  ${tw`lg:flex mb-4 items-center overflow-hidden text-white`}
+`
+const Wrap = styled('div')`
+  ${tw`flex flex-no-wrap m-2 w-auto`}
+`
+const MetricStyles = styled('div')`
+  ${tw`flex flex-wrap flex-auto m-2 justify-center`}
+`
+const Title = styled('div')`
+  ${tw`lg:w-1/3 lg:text-right lg:pr-10 text-center`};
+`
+const Screenshot = styled('div')`
+  ${tw`lg:w-1/2`}
+  img {
+    ${tw`rounded-md max-w-screen-xl`}
+    width: 200%;
+  }
+`
 const Box = styled('div')`
-  ${tw`p-5 w-full lg:max-w-full lg:flex mb-5 pt-16 mt--12`}
+  ${tw`p-5 lg:flex mb-5 pt-16 mt--12 h-auto`}
+  flex: 1 1 600px;
 `
 const Thumbnail = styled('img')`
-  ${tw`h-48 lg:h-40  rounded-lg shadow-md mb-5`}
+  ${tw`h-48 lg:h-40 rounded-lg shadow-md mb-5`}
 `
 const Text = styled('div')`
   ${tw` lg:pl-5 flex flex-col leading-normal lg:text-left`}
@@ -61,36 +77,49 @@ export default function Metrics({ metrics }) {
       />
       {metrics.length ? (
         <div>
-          <Center>
-            <Top>
+          <Top>
+            <Title>
               <H1>Metrics</H1>
               <P>
                 The definitions of all the metrics from your Nozzle dashboard.
               </P>
-            </Top>
-
-            <Container>
-              {metrics.map((metric, i) => {
-                return (
-                  <Box key={metric.fields.fieldId} id={metric.fields.fieldId}>
-                    <Thumbnail
-                      src={metric.fields.thumbnail.fields.file.url}
-                      alt={metric.fields.name}
-                    />
-                    <Text>
-                      <Name href={`#${metric.fields.fieldId}`}>
-                        {metric.fields.name}
-                        <div className="number">{i + 1}</div>
-                      </Name>
-                      <Description>
-                        <Smackdown source={metric.fields.shortDescription} />
-                      </Description>
-                    </Text>
-                  </Box>
-                )
-              })}
-            </Container>
-          </Center>
+            </Title>
+            <Screenshot>
+              <img src="img/metricsScreenshot.png" alt="Nozzle Metrics" />
+            </Screenshot>
+          </Top>
+          <Container>
+            <Center>
+              <Wrap>
+                <MetricStyles>
+                  {metrics.map((metric, i) => {
+                    return (
+                      <Box
+                        key={metric.fields.fieldId}
+                        id={metric.fields.fieldId}
+                      >
+                        <Thumbnail
+                          src={metric.fields.thumbnail.fields.file.url}
+                          alt={metric.fields.name}
+                        />
+                        <Text>
+                          <Name href={`#${metric.fields.fieldId}`}>
+                            {metric.fields.name}
+                            <div className="number">{i + 1}</div>
+                          </Name>
+                          <Description>
+                            <Smackdown
+                              source={metric.fields.shortDescription}
+                            />
+                          </Description>
+                        </Text>
+                      </Box>
+                    )
+                  })}
+                </MetricStyles>
+              </Wrap>
+            </Center>
+          </Container>
         </div>
       ) : (
         ''

e200d6e0f7285fd1fca94f16c1bcdc0354309e66
jake tribe <jacobtribe@me.com>
2020-11-24 10:29:53 -0700

    Update checkout.js

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
+const stripe = Stripe(`${process.env.STRIPE_SECRET_KEY}`)
 
 export default async (req, res) => {
   const session = await stripe.checkout.sessions.create({

da9bb2116a934cf478a69f281207caa20218705b
Jake Tribe <jtribe32@gmail.com>
2020-12-04 11:17:35 -0700

    Fix success page (#125)
    * updates styles to success page
    
    * fix styles

--- pages/paa/success.js
+++ pages/paa/success.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import Link from 'next/link'
-import styled from 'styled-components'
-import Icon from 'components/Icon'
-//
-import { angle } from 'utils/Styles'
+import styled, { css } from 'styled-components'
 
-import { H2, P } from 'components/Html'
+import Head from 'components/Head'
+import { H1, H2, H4, P, Img, Button } from 'components/Html'
 
 const belowMobile = `@media(max-width: ${700}px)`
 
@@ -15,16 +12,55 @@ const Section = ({ children, ...rest }) => (
   </section>
 )
 
+const section = css`
+  z-index: 0;
+  .inner {
+    max-width: ${props => props.theme.maxWidth}px;
+    margin: 0 auto;
+    padding: 5% 10%;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+  img {
+    width: 100%;
+  }
+
+  ${belowMobile} {
+    .left,
+    .right {
+      flex: 1 1 100%;
+      margin-left: 0;
+      margin-right: 0;
+    }
+    .left {
+      margin-bottom: 2rem;
+    }
+  }
+`
+
+const layout = css`
+  .left {
+    flex: 1 1 300px;
+    margin-right: 40px;
+  }
+  .right {
+    flex: 1 1 300px;
+  }
+`
+
 const SectionThanks = styled(Section)`
   z-index: 0;
   .inner {
     max-width: ${props => props.theme.maxWidth}px;
     margin: 0 auto;
-    padding: 10% 10%;
+    padding-top: 8%;
+    padding-left: 10%;
+    padding-right: 10%;
+
     display: flex;
     flex-wrap: wrap;
     align-items: center;
-    min-height: 100vh;
   }
 
   ${belowMobile} {
@@ -39,36 +75,148 @@ const SectionThanks = styled(Section)`
     }
   }
 
-  ${
-    '' /* background: ${props => props.theme.colors.primaryDarker};
-  color: white; */
-  }
-
-  ${angle('right')};
-
-  &:after {
-    display: none;
-  }
-
   .inner {
     display: block;
     text-align: center;
   }
 `
 
+const Example = styled(Section)`
+  ${section};
+  ${layout};
+
+  h2 {
+    width: 100%;
+  }
+
+  .right {
+    img {
+      opacity: 1;
+      max-width: 940px;
+      width: 110%;
+      border-radius: 5px;
+      box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.2);
+    }
+  }
+  .left {
+    padding-right: 1rem;
+
+    h4 {
+      margin-bottom: 2rem;
+    }
+  }
+
+  ${belowMobile} {
+    text-align: center;
+
+    .right {
+      img {
+        width: 100%;
+      }
+    }
+    .left {
+      margin: 0 auto;
+      margin-bottom: 2rem;
+    }
+  }
+`
+
 export default function Success() {
   return (
-    <SectionThanks>
-      <H2 full>Thank you for your purchase!</H2>
-      <P>
-        {' '}
-        We will get your PAA Expansion Deliverable to you as soon as possible!
-      </P>
-      <Link href="/paa">
-        <a className="back">
-          <Icon i="arrow-left" /> Back
-        </a>
-      </Link>
-    </SectionThanks>
+    <div style={{ overflow: 'hidden' }}>
+      <Head
+        title="Payment Successful | Nozzle"
+        description="Thank you for your purchase of the PAA Expansion Deliverable"
+      />
+      <main>
+        <SectionThanks>
+          <H1 full>Thank you for your purchase!</H1>
+          <P>
+            {' '}
+            Your PAA Expansion Deliverable will be delivered to you within 1-2
+            business days. In the meantime, learn what else you can do with
+            Nozzle since we are not your average rank checker. In fact, we are a
+            full SERP monitoring tool which means we have A. Lot. Of. Data.
+          </P>
+        </SectionThanks>
+
+        <Example>
+          <H2>For Example: </H2>
+          <div className="left">
+            <H4>
+              Find which competitors own the most top 3, top 5, or top 10
+              rankings for each of your keyword groups.
+            </H4>
+            <a
+              href="https://nozzle.io/blog/finding-your-top-serp-competitors-across-all-your-keyword-groups"
+              target="blank"
+            >
+              <Button>Learn More</Button>
+            </a>
+          </div>
+          <div className="right">
+            <a
+              href="https://nozzle.io/blog/finding-your-top-serp-competitors-across-all-your-keyword-groups"
+              target="blank"
+            >
+              <Img
+                src="../img/topCompetitors.gif"
+                alt="PAA deliverable list of questions"
+              />
+            </a>
+          </div>
+        </Example>
+        <Example>
+          <div className="left">
+            <H4>
+              We know who all of your competitors are and you can analyze their
+              rankings just as in depth as your own.
+            </H4>
+            <a
+              href="https://nozzle.io/blog/track-serp-rankings-for-an-unlimited-amount-of-competitors-with-nozzle"
+              target="blank"
+            >
+              <Button>Learn More</Button>
+            </a>
+          </div>
+          <div className="right">
+            <a
+              href="https://nozzle.io/blog/track-serp-rankings-for-an-unlimited-amount-of-competitors-with-nozzle"
+              target="blank"
+            >
+              <Img
+                src="../img/monitorCompetitors.gif"
+                alt="PAA deliverable list of questions drilled down"
+              />
+            </a>
+          </div>
+        </Example>
+        <Example>
+          <div className="left">
+            <H4>
+              Monitor your exact position in the SERPs in terms of Pixels from
+              Top in addition to keeping track of your rank.
+            </H4>
+            <a
+              href="https://nozzle.io/blog/pixels-from-top-a-nozzle-feature"
+              target="blank"
+            >
+              <Button>Learn More</Button>
+            </a>
+          </div>
+          <div className="right">
+            <a
+              href="https://nozzle.io/blog/pixels-from-top-a-nozzle-feature"
+              target="blank"
+            >
+              <Img
+                src="../img/pixelsFromtop.png"
+                alt="PAA deliverable graph of daily percentage"
+              />
+            </a>
+          </div>
+        </Example>
+      </main>
+    </div>
   )
 }

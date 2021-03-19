f79ec7971c576d3b8fbbdde88c142d5a13dd8006
jake tribe <jtribe32@gmail.com>
2021-03-19 14:26:42 -0600

    Create Contact Page

---
+++ pages/contact.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro'
+//
+import { angle } from 'utils/Styles'
+
+import { H2, P } from 'components/Html'
+import HubspotForm from 'components/HubspotForm'
+
+const Section = ({ children, ...rest }) => (
+  <section {...rest}>
+    <div className="inner">{children}</div>
+  </section>
+)
+
+const SectionContactUs = styled(Section)`
+  ${angle('right')};
+  ${tw`z-0`}
+  .inner {
+    ${tw`flex flex-wrap items-center mx-auto min-h-screen p-1/10 max-w-default`}
+  }
+  img {
+    ${tw`w-full`}
+  }
+
+  &:after {
+    ${tw`hidden`}
+  }
+
+  .inner {
+    ${tw`block text-center`}
+  }
+`
+
+export default function Contact() {
+  return (
+    <SectionContactUs id="contact">
+      <H2 full>Contact Us</H2>
+      <HubspotForm
+        id="0a2ef4a8-3389-467e-88f7-fb910117cec1"
+        onFormSubmitted={() => {
+          if (typeof window !== 'undefined') {
+            window.dataLayer.push({ event: 'contactUs' })
+          }
+        }}
+      />
+    </SectionContactUs>
+  )
+}

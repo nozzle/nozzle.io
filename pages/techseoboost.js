43690fcf67ed0fe3f1553bad8a55b02b68888520
boydnorwood <52715622+boydnorwood@users.noreply.github.com>
2021-04-21 23:33:44 -0600

    Update techseoboost.js

--- pages/techseoboost.js
+++ pages/techseoboost.js
@@ -117,7 +117,7 @@ export default function TechSeoBoost() {
             <P tw="mb-6">
               Content teams will love our PAA expansion deliverable. <a href="https://datastudio.google.com/reporting/c603c6cc-2137-49b7-a161-735bf238f46d">In one
               example</a> of 500 camping related keywords, we extracted 419 unique
-              questions from the People Also Ask boxes. </p>
+              questions from the People Also Ask boxes. </P>
 
              <P> In another example, we looked at 49,866 SEO related keywords and 
               found 40,894 unique SEO related questions. </P>

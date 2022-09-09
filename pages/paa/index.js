cfc246affb0d64e665c00565ef889e3154ade561
Tanner Linsley <tannerlinsley@gmail.com>
2022-09-09 13:33:06 -0600

    Paa changes (#208)
    * add testing
    
    * remove testing
    
    * use tomorrow
    
    * nevermind
    
    * Update index.js
    
    * Make YouTube video responsive
    
    Tanner, you'd probably do this in a different way, but I don't really understand a lot of what you're doing with the CSS here. My changes work, but feel free to make it better.
    
    * Update index.js
    
    Added all the changes that Jake made.
    
    Co-authored-by: Nathan Blair <nathanburrblair@gmail.com>
    Co-authored-by: Nathan Blair <41348196+nathanburrblair@users.noreply.github.com>

--- pages/paa/index.js
+++ pages/paa/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import styled, { css } from 'styled-components'
 import tw from 'twin.macro'
 import { fetchPaaTestimonials } from '../../contentful'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import HubspotForm from '../../components/HubspotForm'
 //
 import { angle } from 'utils/Styles'
 import Head from 'components/Head'
 
-import { H1, H2, H6, P, Ul, Li, Img, Button } from 'components/Html'
+import { H1, H2, H3, H4, H6, P, Img, Button, Ul, Li } from 'components/Html'
 import { Container, Center } from 'components/Layout'
 
 const belowMobile = `@media(max-width: ${700}px)`
@@ -37,14 +39,14 @@ const section = css`
   }
 `
 
-const layout = css`
+{/*const layout = css`
   .left {
     ${tw`mr-10 flex-300`}
   }
   .right {
     ${tw`flex-300`}
   }
-`
+`*/}
 
 const SectionTitle = styled(Section)`
   ${section};
@@ -57,8 +59,8 @@ const SectionTitle = styled(Section)`
 `
 const SectionTestimonials = styled(Section)`
   ${section};
-  ${angle('right')};
-  ${tw`text-center bg-primaryDarker text-white`}
+
+  ${tw`text-center`}
 
   .title {
     ${tw`w-full mb-16`}
@@ -82,31 +84,18 @@ const SectionTestimonials = styled(Section)`
   }
 `
 
-const SectionKnowWhatQuestions = styled(Section)`
+const SectionIncludes = styled(Section)`
   ${section};
-  ${layout};
-  .right {
-    img {
-      ${tw`opacity-100 w-full rounded shadow-2xl mb-4 max-w-4xl`}
-    }
-  }
-
-  .link {
-    ${tw`text-primaryLighter underline`}
-  }
-
-  ${belowMobile} {
-    .right {
-      img {
-        ${tw`w-0`}
-      }
-    }
-  }
+  ${angle('right')};
+  ${tw`md:(flex justify-center gap-4) bg-primaryDarker text-white text-center`}
 `
 
 const SectionCallToAction = styled(Section)`
   ${section};
+  ${angle('right')};
   text-align: center;
+  ${tw`text-white h-full`}
+  background: rgba(2,21,28,1);
 `
 
 export async function getServerSideProps(ctx) {
@@ -148,78 +137,124 @@ export default function PaaDashBoard({ testimonials }) {
             </Center>
           </Container>
         </SectionTitle>
-        <SectionKnowWhatQuestions>
-          <div className="left">
-            <H2 color="primaryDark">
-              Generate Months of Content Ideas Without any Brainstorming
-              Sessions
-            </H2>
-
-            <P>What you'll get:</P>
-            <P>
-              <Ul>
-                <Li>
-                  An emailed link to a Google Data Studio dashboard containing a
-                  list of hundreds to thousands of People Also Ask questions
-                  related to your target keyword list (depending on how many
-                  keywords you input).
-                </Li>
-              </Ul>
-            </P>
-
-            <br />
-            <P>
-              {' '}
-              <a
-                href="https://datastudio.google.com/u/0/reporting/2fdfc264-1785-46f6-80e4-149ace7336e6/page/T6hmB"
-                target="blank"
-              >
-                <span className="link">
-                  Click to view a PAA Expansion Deliverable example in Data
-                  Studio.
-                </span>
-              </a>{' '}
-            </P>
-
-            <br />
-            <a href="https://app.nozzle.io/sign-up" tw="w-full">
-              <Button
-                color="success"
-                css={`
-                  ${tw`rounded p-4`}
-                `}
-              >
-                Get started!
-              </Button>
-            </a>
+        <div css={tw`h-4`} />
+        <Container>
+          <div css={tw` p-6 md:(flex items-center gap-12)`}>
+            <div css={tw`md:(w-1/2)`}>
+              <H3 color="primaryDark">
+                Generate Months of Content Ideas Without any Brainstorming
+                Sessions
+              </H3>
+              <P>
+                Knowing which People Also Ask questions show up for your target
+                keywords can help you optimize for People Also Ask boxes, give
+                you ideas for blog posts and other content, and guide your SEO
+                strategy. Try our keyword question generator today!
+              </P>
+            </div>
+            <div css={tw`ml-auto`}>
+              <Img
+                src="/img/paaBox.jpg"
+                alt="google’s people also ask boxes help find questions related to your keywords"
+                width="516"
+                height="327"
+              />
+            </div>
           </div>
-          <div className="right">
-            <Img
-              src="/img/paaBox.jpg"
-              alt="google’s people also ask boxes help find questions related to your keywords"
-              width="516"
-              height="327"
-            />
-            <div>
+          <div css={tw`h-4`} />
+          <div css={tw`p-6 md:(flex items-center)`}>
+            <div css={tw` md:(w-1/2) mx-auto text-left`}>
               {/*<div className="wistia_embed wistia_async_ggfgo9uxfs videoFoam=true" />*/}
               <iframe width="560" 
                       height="315" 
                       src="https://www.youtube.com/embed/EKNef5jI7QI" 
                       title="YouTube video player" 
                       frameborder="0" 
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-              </iframe>
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                      allowfullscreen></iframe>
+            </div>
+            <div css={tw`md:(w-1/2  gap-20 pl-16)`}>
+              <H3 color="primaryDark">What You'll Get</H3>
+              <P>
+                An emailed link to a Google Data Studio dashboard containing a
+                list of hundreds to thousands of People Also Ask questions
+                related to your target keyword list (depending on how many
+                keywords you input).
+              </P>
+              <br />
+              <P>
+                {' '}
+                <a
+                  href="https://datastudio.google.com/u/0/reporting/2fdfc264-1785-46f6-80e4-149ace7336e6/page/T6hmB"
+                  target="blank"
+                >
+                  <span css={tw`text-primaryLighter underline`}>
+                    Click to view a PAA Expansion Deliverable example in Data
+                    Studio.
+                  </span>
+                </a>{' '}
+              </P>
+              <br />
+              <H3 color="primaryDark">How to Get Started </H3>
+              <Ul>
+                <Li>
+                Create a Nozzle account (This creates a workspace on the back
+                  end so we have a place to run your keywords through our
+                  question generator.)
+                </Li>
+                <Li>Input your name, email address, company name and domain</Li>
+                <Li>Input your target keyword list (10,000 keywords max)</Li>
+              </Ul>
+              <br />
+              <AnchorLink href="#signup">
+                <Button
+                  color="success"
+                  css={`
+                    ${tw`rounded p-4`}
+                  `}
+                >
+                  Get started!
+                </Button>
+              </AnchorLink>
             </div>
           </div>
-        </SectionKnowWhatQuestions>
-        {/* <div tw="w-full min-h-screen text-center">
-          <iframe
-            title="PAA sign up"
-            src="https://dev.nozzle.io/paa"
-            width="50%"
-            height="1000rem"
-          />
-        </div> */}
+          <div css={tw`h-16`} />
+        </Container>
+        <SectionIncludes>
+          <div css={tw`md:(w-1/3)`}>
+            <div css={tw`md:(px-6)`}>
+              <H6 css={tw`font-bold`}>
+                Discover the Questions Your Audience Is Asking
+              </H6>
+              <P css={tw`text-sm`}>
+                For your content to be effective, it must answer your audience's
+                questions. Our PAA deliverable does the work of finding those
+                questions for you.
+              </P>
+            </div>
+          </div>
+          <div css={tw`md:(w-1/3)`}>
+            <div css={tw`md:(px-6)`}>
+              <H6 css={tw`font-bold`}>See How Often Questions Appear</H6>
+              <P css={tw`text-sm`}>
+                Our PAA deliverable shows how often questions appeared in PAA
+                when people searched your keywords, guiding you to the popular
+                questions you should answer first.
+              </P>
+              </div>
+              </div>
+              <div css={tw`md:(w-1/3)`}>
+            <div css={tw`md:(px-6)`}>
+              <H6 css={tw`font-bold`}>See PAA Frequency by Keyword Group</H6>
+              <P css={tw`text-sm`}>
+                If you provide keyword groups with your keyword list, our
+                deliverable will show you how often keyword groups show PAA
+                boxes, letting your content team know which groups to focus on
+                first.
+              </P>
+            </div>
+          </div>
+        </SectionIncludes>
         <SectionTestimonials>
           <H2 className="title">What People Are Saying</H2>
 
@@ -246,26 +281,16 @@ export default function PaaDashBoard({ testimonials }) {
             )
           })}
         </SectionTestimonials>
-        <SectionCallToAction>
-          <H2 full>Get Your PAA Deliverable Today!</H2>
+        <SectionCallToAction id="signup">
+          <H2>Get Your PAA Deliverable Today!</H2>
           <div
             css={`
-              ${tw`w-full `}
+              ${tw`w-full`}
             `}
           >
-            <a href="https://app.nozzle.io/sign-up">
-              <Button
-                color="success"
-                css={`
-                  ${tw`text-2xl rounded p-6 `}
-                `}
-              >
-                Get started!
-              </Button>
-            </a>
+            <HubspotForm id="e4275bbe-094f-4443-9f3c-ec10caa772e6" />
           </div>
         </SectionCallToAction>
-        {/* Hello world */}
       </main>
     </div>
   )
